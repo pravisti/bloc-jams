20813689674bcd39c0f3d4a010b30030519e5b66
Pravisti <pravisti@gmail.com>
2015-12-10 17:50:28 -0800

    Final changes

---
+++ styles/test.js
@@ -0,0 +1,5 @@
+var myName = Pravisti,
+    courseTrack = Design, 
+     courseType = Front End Web Development;
+
+console.log ( "My name is"+ myName + " and I'm taking Bloc's" + courseType +  "course on the " + courseTrack + "track"); 
