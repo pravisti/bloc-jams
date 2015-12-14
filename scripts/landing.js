a1b8fd6a0a1861bfe788028c5df23849698dcd5a
Pravisti <pravisti@gmail.com>
2015-12-14 15:16:31 -0800

    assignment 23 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,26 @@
 var pointsArray = document.getElementsByClassName('point');
  
- var animatePoints = function(points) {
 	 
-     var revealPoint = function(index) {
-         points[index].style.opacity = 1;
-         points[index].style.transform = "scaleX(1) translateY(0)";
-         points[index].style.msTransform = "scaleX(1) translateY(0)";
-         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+     var revealPoint = function(point) {
+         points.style.opacity = 1;
+         points.style.transform = "scaleX(1) translateY(0)";
+         points.style.msTransform = "scaleX(1) translateY(0)";
+         points.style.WebkitTransform = "scaleX(1) translateY(0)";
      };
- 
-    for(var i = 0; i< points.length;i++){
-		revealPoint(i);
-	}
- 
+	 
+ var animatePoints = function(points) {
+	 forEach(points,revealPoint);
  };
 
 window.onload = function() {
 	 if (window.innerHeight > 950) {
          animatePoints(pointsArray);
      }
-	 var sellingPoints = document.getElementsByClassName('selling-points')[0];
-	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
-      window.addEventListener('scroll', function(event) {
-  if (document.body.scrollTop >= scrollDistance) {
+      window.addEventListener("scroll", function(event) {
+  if (pointsArray[0].getBoundingClientRect().top <= 500) {
              animatePoints(pointsArray);   
          }
      });
  }
+
