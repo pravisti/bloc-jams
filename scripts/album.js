3518237e43ff645f14549a6d81e88e301e6b774f
Pravisti <pravisti@gmail.com>
2015-12-14 16:56:34 -0800

    assignment-25-complete

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,22 @@
      ]
  };
 
+var albumEminem = {
+     name: 'Slim Shady',
+     artist: 'Eminem',
+     label: 'DRE',
+     year: '2000',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { name: 'Blue', length: '4:26' },
+         { name: 'Green', length: '3:14' },
+         { name: 'Red', length: '5:01' },
+         { name: 'Pink', length: '3:21'},
+         { name: 'Magenta', length: '3:15'}
+     ]
+ };
+ 
+
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -42,15 +58,15 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
-var setCurrentAlbum = function(album) {
      // #1
      var albumTitle = document.getElementsByClassName('album-view-title')[0];
      var albumArtist = document.getElementsByClassName('album-view-artist')[0];
      var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
      var albumImage = document.getElementsByClassName('album-cover-art')[0];
      var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
-     // #2
+
+var setCurrentAlbum = function(album) {
+	// #2
      albumTitle.firstChild.nodeValue = album.name;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -67,4 +83,14 @@ var setCurrentAlbum = function(album) {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+	 
+	 var albums =[albumPicasso, albumEminem, albumMarconi];
+	 var index = 1;
+	 albumImage.addEventListener("click", function(event) {
+		 setCurrentAlbum(albums[index]);
+		 index ++;
+if (index ==albums.length) {
+index = 0;
+}
+	 });
  };
